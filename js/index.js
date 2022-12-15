274cf05c7605b728ef92938ced844b4b1d506103
32MISOchipmunk <terry20070415@gmail.com>
2022-12-15 21:24:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||寶|貝|祝|你|生|日|快|樂|#heart|');
+      S.UI.simulate('|#countdown 3||寶|貝|祝|你|生|日|快|樂|#rectangle|');
     }
 
     S.Drawing.loop(function () {
